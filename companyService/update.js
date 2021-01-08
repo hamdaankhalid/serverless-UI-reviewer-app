f3cc71cb818551650fa45425c0959176ab11ef84
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2021-01-08 15:32:51 -0500

    restructuring service

---
+++ companyService/update.js
@@ -0,0 +1,24 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        Key:{
+            ComapnyId: event.requestContext.identity.cognitoIdentityId,
+            UiId: event.pathParameters.id,
+        },
+        UpdateExpression: "Set content = :content, attachment= :attachment",
+        ExpressionAttributeValues: {
+            ":task": data.task || null, // Parsed from request body
+            ":attachment": data.attachment || null, // Parsed from request body
+            ":link": data.link || null, //parsed from request body
+        },
+        ReturnValues: "ALL_NEW",
+    };
+
+    await dynamoDb.update(params);
+
+    return { status: true };
+});
